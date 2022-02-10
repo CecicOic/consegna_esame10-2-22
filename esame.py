735d0cf69a9199922d901d1dd22fb36a2d3338d5
MARCO SECCI <marcosecci2002@gmail.com>
2022-02-10 11:04:17 +0100

    Removed main

--- esame.py
+++ esame.py
@@ -256,6 +256,6 @@ def compute_avg_monthly_difference(time_series, first_year, last_year):
 #  MAIN  #
 # ====== #
 
-time_series_file = CSVTimeSeriesFile(name='data.csv')
-time_series = time_series_file.get_data()
-print(compute_avg_monthly_difference(time_series, '1949', '1953'))
+# time_series_file = CSVTimeSeriesFile(name='data.csv')
+# time_series = time_series_file.get_data()
+# print(compute_avg_monthly_difference(time_series, '1949', '1953'))
